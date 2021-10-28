218a4f5f94e3857c2ed24e33d9d07e1187a67966
alaa140101 <alaa140011@gamil.com>
2021-10-28 16:50:08 +0300

    fix error

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\User as UserResource;
 
